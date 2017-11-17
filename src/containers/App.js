60eaf8e2cb52af393a51d612906f5ad190be601a
Trakton <higor.cmbotelho@gmail.com>
2017-11-17 14:55:29 -0300

    Adding blank about webpage

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import logo from './../images/logo.svg';
-import './App.css';
+import About from './About';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={About}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
